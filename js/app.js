c578ec7cd6eb178c9ab71955b29508ac72fce14f
MariaCravioto <maycravis@gmail.com>
2017-12-30 05:53:18 -0600

    responsive

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){//giving a function to the jquery
+$("ul.tabs li a:first").addClass("active");//adding active class to the first li
+$(".text article").hide();//hide the sections
+$(".text article:first").show();//show the first article
+
+$("ul.tabs li a").click(function(){
+  $("ul.tabs li a").removeClass("active");
+  $(this).addClass("active");//add the class to each elect chosen
+  $(".text article").hide();
+
+var activeTab = $(this).attr("href");//we create a variable whose value will be the href attribute
+$(activeTab).show();
+return false;
+});
+});
